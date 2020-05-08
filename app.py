24df797e1e44932f2083e77376a14d6aedb84412
weixun2019 <xw2539@columbia.edu>
2020-05-07 22:25:08 -0400

    update

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 14:57:17 2020
+
+@author: etill
+"""
+
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/assignments")
+def assignments():
+    return render_template("assignments.html")
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+#start the server
+if __name__ == "__main__":
+    app.run()
